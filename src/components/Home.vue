e76c704dbb084b5564ebca8c9638730d53ea4800
austin <austin89peterson@gmail.com>
2018-11-29 16:46:35 -0700

    HOOKED UP BACKEND!!!!!!!! BOOM!!!!

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,28 @@
 <template>
-    <div id="home">
-        <h3> home page</h3>
-        <v-btn
-        flat
-        href="/login"
-        target="login here"
-      >
-        <span class="mr-2">Enter the Store</span>
-      </v-btn>
-    <div component = 'dashboard'>
-        </div>  
+  <div id="home">
+    <h3>home page</h3>
+    <v-btn flat href="/login" target="login here">
+      <span class="mr-2">Enter the Store</span>
+    </v-btn>
+    <div component="dashboard">
+        <ul>
+      <li v-for="user in users" v-bind:key="user.id">
+        id: {{user.id}}
+      </li>
+    </ul>
     </div>
+  </div>
 </template>
 
 
 <script>
+const db = firebase.firestore();
+console.log(db);
 export default {
-    name: 'home',
-    data () {
-        return {
-        }
-    }
-}
+  name: "home",
+  data: () => ({ users: [] }),
+  firestore: {
+    users: db.collection("users")
+  }
+};
 </script>
