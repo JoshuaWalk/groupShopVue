d14f8b13695c474c7b420d42a3515c52074c84b6
austin <austin89peterson@gmail.com>
2018-11-30 10:30:16 -0700

    Backend hooked to Shop

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,16 +1,28 @@
 <template>
-    <dive id="shop">
-        <h3> this is the shop </h3>
-    </dive>
+    <center>
+        <div id="shop">
+            <h3> this is the shop </h3>
+
+        <li v-for="mens in tshirts" v-bind:key="tshirts.id">
+        id: {{mens.id}}
+        </li>
+
+        <button onclick
+        
+        </div>
+    </center>
 </template>
 
 
+
 <script>
+const db = firebase.firestore();
+console.log(db);
 export default {
-    name: 'shop',
-    data () {
-        return {
-        }
-    }
-}
+  name: "shop",
+  data: () => ({ tshirts: [] }),
+  firestore: {
+    tshirts: db.collection("tshirts")
+  }
+};
 </script>
